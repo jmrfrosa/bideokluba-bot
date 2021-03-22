08b735d462e8d09824ca1b6fc93b5964492f7855
João Rosa <jmrfrosa92@gmail.com>
2021-03-22 19:04:46 +0000

    database, movie & misc packages

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
+const appPath = path.resolve(__dirname);
+
 module.exports = {
+  appPath,
   prefix: "!",
-  token: process.env.BOT_TOKEN
+  token: process.env.BOT_TOKEN,
+  omdbToken: process.env.OMDB_TOKEN,
+  dbPath: path.join(appPath, '/db/datastore.db')
 }
