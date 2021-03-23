aa76031acbefd1b3666cc2a1800f8b68edb6df74
João Rosa <jmrfrosa92@gmail.com>
2021-03-23 23:37:21 +0000

    revamped results command

--- commands/resultados.js
+++ commands/resultados.js
@@ -2,13 +2,15 @@ const { client } = require('../util/client.js');
 const { parseMessageId } = require("../util/common");
 const { roles } = require('../util/constants.js');
 
+const validReactions = ['👍', '👎'];
+
 module.exports = {
   name: 'resultados',
   description: 'Finalizar uma mensagem de voto. Devolve os resultados.',
   args: true,
   usage: '<poll_url>',
   guildOnly: true,
-  execute(message, args) {
+  async execute(message, args) {
     const hasRole = message.member.roles.cache.some(role => ([roles.admin, roles.curator].includes(role.name)));
 
     if (!hasRole) {
@@ -23,10 +25,45 @@ module.exports = {
 
     const pollId = parseMessageId(args[0]);
     const poll = client.polls.get(pollId);
+
+    if (!poll) {
+      message.reply('Esta votação não existe ou não se encontra activa!');
+      return;
+    }
+
     const report = poll.report();
 
-    poll.end();
+    const reportMsg = await message.channel.send(report);
+    const removeMsg = await message.channel.send('Queres terminar esta votação?');
 
-    message.channel.send(report);
+    const reactionPromises = validReactions.map((reaction) => removeMsg.react(reaction));
+    await Promise.all(reactionPromises);
+
+    const listener = removeMsg.createReactionCollector(reactionFilter, { time: 15000 });
+
+    listener.on('collect', (reaction, _user) => {
+      const { name } = reaction.emoji;
+
+      switch (name) {
+        case '👍':
+          poll.end();
+          removeMsg.edit('A votação foi terminada!');
+          reportMsg.edit(`A votação terminou.\n${reportMsg.content}`);
+          break;
+        case '👎':
+          removeMsg.delete();
+          break;
+        default:
+          break;
+      }
+    });
+
+    listener.on('end', (_reaction, _user) => {
+      removeMsg.delete();
+    });
   }
+}
+
+function reactionFilter(reaction, _user) {
+  return validReactions.includes(reaction.emoji.name);
 }
