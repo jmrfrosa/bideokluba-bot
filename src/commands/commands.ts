83efd5e00d87b410c173cde7fcf1e750adab62f7
João Rosa <jmrfrosa92@gmail.com>
2022-08-09 00:48:26 +0100

    feat(v13): remove unused commands

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,15 +2,15 @@ import { CommandInterface } from '@typings/command.type'
 import { EventoCommand } from './events/evento.command'
 import { AdminCommand } from './management/admin.command'
 import { AniversáriosCommand } from './management/aniversarios.command'
-import { AjudaCommand } from './misc/ajuda.command'
 import { CancelarCommand } from './misc/cancelar.command'
+import { MoviesCommand } from './movies/movies.command'
 import { VotaçãoCommand } from './polls/votação.command'
 
 export const commandList: CommandInterface[] = [
   EventoCommand,
   VotaçãoCommand,
   AdminCommand,
-  AjudaCommand,
   CancelarCommand,
   AniversáriosCommand,
+  MoviesCommand,
 ]
